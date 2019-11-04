8db9da79935a884e207bc87b07830f7b95a2b3ea
Do Tung Duong <duong98kk@gmail.com>
2019-11-04 11:32:38 +0700

    'add-ranking-chart'

---
+++ back-end/Data/DataRankingChart.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { x: "PUBG", y: 50000000 },
+  { x: "Minecraft", y: 30000000 },
+  { x: "Diablo III", y: 20000000 },
+  { x: "WOW", y: 14000000 },
+  { x: "Terraria", y: 12000000 },
+  { x: "The Sims", y: 11000000 }
+]
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,17 +3,19 @@ import * as actions from './actions/index';
 import { connect } from 'react-redux';
 import './App.css';
 import PieChart from './components/PieChart'
+import RankingChart from './components/RankingChart'
 
 class App extends Component {
   componentDidMount() {
     this.props.getApi();
   }
   render() {
-    const { dataPieChart } = this.props
+    const { dataPieChart, dataRankingChart } = this.props
     const colorArr = ["tomato", "orange", "gold", "cyan", "navy", "green"]
     return (
       <div className='App'>
         <PieChart Data={dataPieChart.data} checkResponse={dataPieChart.checkResponse} colorArr={colorArr} />
+        <RankingChart Data={dataRankingChart.data} checkResponse={dataRankingChart.checkResponse} />
       </div>
     );
   }
@@ -21,7 +23,8 @@ class App extends Component {
 
 const mapStatetoProps = (state) => {
   return {
-    dataPieChart: state.dataPieChart
+    dataPieChart: state.dataPieChart,
+    dataRankingChart: state.dataRankingChart
   }
 }
 const mapDispatchToProps = (dispatch) => {
