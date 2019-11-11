c90a3bfaef08a7067a8afccd4a78bbd75b9c0af8
Your Name <you@example.com>
2019-11-11 22:20:51 +0700

    fix-all-bug

--- front-end/src/components/DateRangePicker.js
+++ front-end/src/components/DateRangePicker.js
@@ -5,21 +5,40 @@ import "react-dates/initialize";
 import { DateRangePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
 import './DateRangePicker.css'
+import { Button } from 'reactstrap'
 
 function DatePicker(props) {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [focusedInput, setFocusedInput] = useState(null);
+  const [CatchStartAccept, setCatchStartAccept] = useState('');
+  const [CatchEndAccept, setCatchEndAccept] = useState('');
   const handleDatesChange = ({ startDate, endDate }) => {
     setStartDate(startDate);
     setEndDate(endDate);
     if (startDate !== null && endDate !== null) {
-      props.setDateRangePicker(String(startDate._d), String(endDate._d))
+      props.setDateRangePicker(startDate.format('L'), endDate.format('L'))
     }
   };
-  const endPick = () => {
+  const renderButton = () => <div className='group-btn'>
+    <Button outline color="success" onClick={Accept}>Accept</Button>
+    <Button outline color="danger" onClick={Cancel}>Cancel</Button>
+  </div>
+  const Accept = () => {
+    if (startDate === null || endDate === null) {
+      return alert("You can't leave it blank")
+    }
+    if (startDate.format('L') === CatchStartAccept && endDate.format('L') === CatchEndAccept) {
+      return alert("You can't let them duplicate")
+    }
+    setCatchStartAccept(startDate.format('L'))
+    setCatchEndAccept(endDate.format('L'))
     props.showLoading();
     props.getApi(props.dataDateRangePicker);
+    setFocusedInput(null) // để null thì đóng calendar
+  }
+  const Cancel = () => {
+    setFocusedInput(null) // để null thì đóng calendar
   }
   return (
     <div className="DateRangePicker">
@@ -31,7 +50,9 @@ function DatePicker(props) {
         onDatesChange={handleDatesChange}
         focusedInput={focusedInput}
         onFocusChange={focusedInput => setFocusedInput(focusedInput)}
-        onClose={endPick}
+        renderCalendarInfo={renderButton}
+        keepOpenOnDateSelect={true} //giữ cho calendar không đóng khi click đủ 2 input
+      // withFullScreenPortal={true} //bâm vào thì full màn hình
       />
     </div>
   );
